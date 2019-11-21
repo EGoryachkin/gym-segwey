2ebc2bc6c47b165f752ce7059219131562edad3b
EGoryachkin <egor.goryachkin@moley.com>
2019-11-21 23:52:14 +0300

    fix init

--- gym_segwey/envs/__init__.py
+++ gym_segwey/envs/__init__.py
@@ -1,2 +1,2 @@
 from gym_segwey.envs.segwey_env import SegweyEnv
-from gym_segwey.envs.segwey_extrahard_env import SegweyExtraHardEnv
+# from gym_segwey.envs.segwey_extrahard_env import SegweyExtraHardEnv
